fd0cea709985b815ed1e6df9996772f280c2598f
Tadej Rola <tadej.rola@student.um.si>
2018-07-05 09:41:23 +0200

    heroku deploy fix

--- dbHelpers/connection.js
+++ dbHelpers/connection.js
@@ -1,17 +1,10 @@
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
-    host: '164.8.250.122',
-    user: 'pkp_bc',
-    password: 'pkp_bc',
-    database: 'pkp_bc'
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
-// var getConnection = function (callback) {
-//     pool.getConnection(function (err, connection) {
-//         callback(err, connection);
-//     });
-// };
-
-// module.exports = getConnection;
 exports.pool = pool;
